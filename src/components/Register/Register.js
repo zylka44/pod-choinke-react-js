bd7d3972c875a653ec47cac628443a9ad4b34997
zylka44 <zancza@gmail.com>
2021-07-03 19:22:29 +0200

    Change api endpoint

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onRegister = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://radiant-plateau-91423.herokuapp.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://radiant-plateau-91423.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
